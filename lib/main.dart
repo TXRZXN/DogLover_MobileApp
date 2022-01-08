a80337d5f2df0347aad39d108daea5dafa623899
TXRZXN <tarzanmsss1999@gmail.com>
2022-01-08 22:11:56 +0700

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:doglovers/screen/questionnaire/result2.dart';
 
-
+//hahaha//
 
 String initRoute = '/login';
 
